e87f87f0231c2d2688870149b8b26c9402c56e10
Andreas Hove <andreas.h@live.no>
2017-09-12 10:29:07 +0200

    239-aGameOfThrees completed.

---
+++ 239-Tests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("239-Tests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("239-Tests")]
+[assembly: AssemblyCopyright("Copyright ©  2017")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("c4c717cd-9ae5-431b-a254-895a8070d39e")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
--- 239-aGameOfThrees/Program.cs
+++ 239-aGameOfThrees/Program.cs
@@ -10,6 +10,88 @@ namespace _239_aGameOfThrees
     {
         static void Main(string[] args)
         {
+            string _ans;
+            string _in;
+            int _number;
+            bool _notDone = true;
+
+            Console.WriteLine("Game of Threes! Input a number (integer) to be divided by 3 until it is 1\n");
+                
+
+            while (_notDone)
+            {
+                Console.Write("Input number/integer: ");
+                _in = Console.ReadLine();
+                if (String.IsNullOrEmpty(_in))
+                {
+                    Console.Write("Continue? (y/n): ");
+                    _ans = Console.ReadLine();
+                    if (_ans == "n")
+                        _notDone = false;
+                    Console.WriteLine("");
+                    continue;
+                }
+                    
+
+                try
+                {
+                    _number = int.Parse(_in);
+                }
+                catch (FormatException)
+                {
+                    Console.WriteLine("Error: \""+_in+"\" is not an integer.");
+                    continue;
+                }
+                catch (OverflowException)
+                {
+                    Console.WriteLine("Error: \"" + _in + "\" is too large or too small.");
+                    continue;
+                }
+                
+
+                Divide(_number);
+
+                Console.WriteLine("");
+                Console.Write("Continue? (y/n): ");
+                _ans = Console.ReadLine();
+                if (_ans == "n")
+                    _notDone = false;
+                Console.WriteLine("");
+
+            }
+            
+        }
+
+        private static void Divide(int n)
+        {
+            Console.Write(n);
+
+            if (n == 1 || n == -1)
+                return;
+            
+
+            if (n % 3 == 0)
+            {
+                Console.WriteLine("");
+                n = n/3;
+                
+            }
+            else if ((n + 1) % 3 == 0)
+            {
+                Console.WriteLine(" + 1");
+                n = (n + 1)/3;
+                
+
+            }
+            else if ((n - 1) % 3 == 0)
+            {
+                Console.WriteLine(" - 1");
+                n = (n - 1)/3;
+
+            }
+            
+
+            Divide(n);
         }
     }
 }
