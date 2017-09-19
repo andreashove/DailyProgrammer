4929e5dd9cb9bbda18e5fbcb373165621e6221d3
Andreas Hove <andreas.h@live.no>
2017-09-19 08:44:44 +0200

    Implemented DefuseTheBomb game

--- 321-talkingClock/Program.cs
+++ 321-talkingClock/Program.cs
@@ -2,17 +2,24 @@
 using System;
 using System.Collections.Generic;
 using System.IO;
+using System.Speech.Synthesis;
 
 namespace _321_talkingClock
 {
     class Program
     {
+        
         static void Main(string[] args)
         {
             string _inputTimestamp;
             var _hours = LoadJson();
 
+            SpeechSynthesizer s = new SpeechSynthesizer();
+            s.SelectVoice("Microsoft Zira Desktop");
             
+
+          
+
 
             Dictionary<string, string> _ones = new Dictionary<string, string>{
                 { "0", "" },
@@ -58,72 +65,81 @@ namespace _321_talkingClock
 
             while (_notDone)
             {
-                string _inHour = "";
-                string _inMinute = "";
-                string _minuteTens = "";
-                string _minuteOnes = "";
-                string _amOrPm = "";
-                string minutes2 = "";
-                string ones = "";
-
-                Console.Write("Input a time (hh:mm) or hit Enter to get current time: ");
-                _inputTimestamp = Console.ReadLine();
-
-                if (_inputTimestamp == "")
-                {
-                    _inputTimestamp = DateTime.Now.Hour.ToString("00.##")+":"+DateTime.Now.Minute.ToString("00.##");
-                }
-                else if (_inputTimestamp.Length != 5 || _inputTimestamp[2] != ':')
-                {
-                    Console.WriteLine("Error: \"" + _inputTimestamp + "\" is not in the valid format \"hh:mm\"");
-                    continue;
-                }
-
-                string[] timestamp = _inputTimestamp.Split(':');
-
-                _inHour = timestamp[0];
-                _inMinute = timestamp[1];
-
-                _hours.TryGetValue(_inHour, out string oaut);
-                if (int.Parse(_inHour) >= 0 && int.Parse(_inHour) < 12)
-                    _amOrPm = "am";
-                else
-                    _amOrPm = "pm";
-
-
-
-                if (_inMinute != "00")
-                {
-                    
-                    _minuteTens = "" + _inMinute[0];
-                    _minuteOnes = "" + _inMinute[1];
-
-                    if (int.Parse(_inMinute) >= 10 && int.Parse(_inMinute) < 20)
-                    {
-                        _tensOfOnes.TryGetValue(_inMinute, out string minutes);
-                        minutes2 = minutes;
-                    }
-                    else
-                    {
-                        _tens.TryGetValue(_minuteTens, out string minutes);
-                        minutes2 = minutes;
-                        _ones.TryGetValue(_minuteOnes, out string outOnes);
-                        ones = outOnes;
-                    }
-
-                    Console.WriteLine("It's " + oaut + " " + minutes2 + " " + ones + " " + _amOrPm);
-                }
-                else
-                {
-                    Console.WriteLine("It's " + oaut + " " + _amOrPm);
-
-                }
+                ConvertTimestampToSpeech();
+                
 
 
             }
 
 
 
+        }
+
+        private static void ConvertTimestampToSpeech()
+        {
+            string _inHour = "";
+            string _inMinute = "";
+            string _minuteTens = "";
+            string _minuteOnes = "";
+            string _amOrPm = "";
+            string minutes2 = "";
+            string ones = "";
+
+            Console.Write("Input a time (hh:mm) or hit Enter to get current time: ");
+            _inputTimestamp = Console.ReadLine();
+
+            if (_inputTimestamp == "")
+            {
+                _inputTimestamp = DateTime.Now.Hour.ToString("00.##") + ":" + DateTime.Now.Minute.ToString("00.##");
+            }
+            else if (_inputTimestamp.Length != 5 || _inputTimestamp[2] != ':')
+            {
+                Console.WriteLine("Error: \"" + _inputTimestamp + "\" is not in the valid format \"hh:mm\"");
+                s.Speak(_inputTimestamp + " is not in the valid format");
+                continue;
+            }
+
+            string[] timestamp = _inputTimestamp.Split(':');
+
+            _inHour = timestamp[0];
+            _inMinute = timestamp[1];
+
+            _hours.TryGetValue(_inHour, out string oaut);
+            if (int.Parse(_inHour) >= 0 && int.Parse(_inHour) < 12)
+                _amOrPm = "a.m.";
+            else
+                _amOrPm = "p.m.";
+
+
+
+            if (_inMinute != "00")
+            {
+
+                _minuteTens = "" + _inMinute[0];
+                _minuteOnes = "" + _inMinute[1];
+
+                if (int.Parse(_inMinute) >= 10 && int.Parse(_inMinute) < 20)
+                {
+                    _tensOfOnes.TryGetValue(_inMinute, out string minutes);
+                    minutes2 = minutes;
+                }
+                else
+                {
+                    _tens.TryGetValue(_minuteTens, out string minutes);
+                    minutes2 = minutes;
+                    _ones.TryGetValue(_minuteOnes, out string outOnes);
+                    ones = outOnes;
+                }
+
+                Console.WriteLine("It's " + oaut + " " + minutes2 + " " + ones + " " + _amOrPm);
+                s.Speak("The time is " + oaut + " " + minutes2 + " " + ones + " " + _amOrPm);
+            }
+            else
+            {
+                Console.WriteLine("It's " + oaut + " " + _amOrPm);
+                s.Speak("The time is " + oaut + " " + _amOrPm);
+
+            }
         }
 
         static Dictionary<string,string> LoadJson()
