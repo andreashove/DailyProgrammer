2eb40be8701398342968886cd2ddbec288a7c629
hove <andreas.hove@avoconsulting.no>
2019-03-19 21:53:16 +0100

    Solved the challenge rather lazy
    Deleted an accidentally created vb net project

--- 375-printANewNumber/Program.vb
+++ 375-printANewNumber/Program.vb
@@ -2,6 +2,13 @@ Imports System
 
 Module Program
     Sub Main(args As String())
+
+        Console.WriteLine("A number is input in computer then a new no should get printed by adding one to each of its digit:");
+        Console.Read();
+
+
+
+
         Console.WriteLine("Hello World!")
     End Sub
 End Module
---
+++ 375-printANewNumberCsharp/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace _375_printANewNumberCsharp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                Console.WriteLine("A number is input in computer then a new no should get printed by adding one to each of its digit:");
+                var input = Console.ReadLine();
+                int value;
+                var result = "";
+                if (int.TryParse(input, out value))
+                {
+                    var inputArr = input.ToCharArray();
+                    foreach (Char c in inputArr)
+                    {
+                        int converted;
+                        int.TryParse(c.ToString(), out converted);
+                        converted += 1;
+                        result += converted.ToString();
+                    }
+                    Console.WriteLine("Result: " + result);
+                    Console.WriteLine("Any key to quit");
+                    Console.Read();
+                    Environment.Exit(0);
+
+
+                }
+                else
+                {
+                    continue;
+                }
+            }
+        }
+    }
+}
